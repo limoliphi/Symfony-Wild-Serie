f26083e8690696ad1a4d618f5e656ae8ccb0cf4c
Lilian Maurin <lilianmaurin.wcs2019@gmail.com>
2019-11-19 08:42:50 +0100

    ajout webpack encore

--- var/cache/dev/twig/de/dee64441c20bebdc14f3315679a7401e3ee25a55900170b30d35d3c431b6a0ee.php
+++ var/cache/dev/twig/de/dee64441c20bebdc14f3315679a7401e3ee25a55900170b30d35d3c431b6a0ee.php
@@ -53,15 +53,21 @@ class __TwigTemplate_0f2ebf2e8da89324b9f34a4545d83070e7f95f205be227a04122392f19f
     ";
         // line 7
         $this->displayBlock('stylesheets', $context, $blocks);
-        // line 8
+        // line 10
         echo "</head>
+<header>
+    ";
+        // line 12
+        $this->loadTemplate("wild/navbar.html.twig", "base.html.twig", 12)->display($context);
+        // line 13
+        echo "</header>
 <body>
 ";
-        // line 10
+        // line 15
         $this->displayBlock('body', $context, $blocks);
-        // line 11
+        // line 16
         $this->displayBlock('javascripts', $context, $blocks);
-        // line 12
+        // line 19
         echo "</body>
 </html>";
         
@@ -89,12 +95,17 @@ class __TwigTemplate_0f2ebf2e8da89324b9f34a4545d83070e7f95f205be227a04122392f19f
         $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
         $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "stylesheets"));
 
+        // line 8
+        echo "        ";
+        echo $this->extensions['Symfony\WebpackEncoreBundle\Twig\EntryFilesTwigExtension']->renderWebpackLinkTags("app");
+        echo "
+    ";
         
         $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
 
     }
 
-    // line 10
+    // line 15
     public function block_body($context, array $blocks = [])
     {
         $macros = $this->macros;
@@ -106,13 +117,18 @@ class __TwigTemplate_0f2ebf2e8da89324b9f34a4545d83070e7f95f205be227a04122392f19f
 
     }
 
-    // line 11
+    // line 16
     public function block_javascripts($context, array $blocks = [])
     {
         $macros = $this->macros;
         $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
         $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "javascripts"));
 
+        // line 17
+        echo "    ";
+        echo $this->extensions['Symfony\WebpackEncoreBundle\Twig\EntryFilesTwigExtension']->renderWebpackScriptTags("app");
+        echo "
+";
         
         $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
 
@@ -123,9 +139,14 @@ class __TwigTemplate_0f2ebf2e8da89324b9f34a4545d83070e7f95f205be227a04122392f19f
         return "base.html.twig";
     }
 
+    public function isTraitable()
+    {
+        return false;
+    }
+
     public function getDebugInfo()
     {
-        return array (  110 => 11,  98 => 10,  86 => 7,  73 => 6,  65 => 12,  63 => 11,  61 => 10,  57 => 8,  55 => 7,  51 => 6,  44 => 1,);
+        return array (  128 => 17,  121 => 16,  109 => 15,  99 => 8,  92 => 7,  79 => 6,  71 => 19,  69 => 16,  67 => 15,  63 => 13,  61 => 12,  57 => 10,  55 => 7,  51 => 6,  44 => 1,);
     }
 
     public function getSourceContext()
@@ -136,11 +157,18 @@ class __TwigTemplate_0f2ebf2e8da89324b9f34a4545d83070e7f95f205be227a04122392f19f
     <meta charset=\"UTF-8\">
     <link rel=\"stylesheet\" href=\"build/css/app.css\">
     <title>{% block title %}Welcome!{% endblock %}</title>
-    {% block stylesheets %}{% endblock %}
+    {% block stylesheets %}
+        {{ encore_entry_link_tags('app') }}
+    {% endblock %}
 </head>
+<header>
+    {% include 'wild/navbar.html.twig' %}
+</header>
 <body>
 {% block body %}{% endblock %}
-{% block javascripts %}{% endblock %}
+{% block javascripts %}
+    {{ encore_entry_script_tags('app') }}
+{% endblock %}
 </body>
 </html>", "base.html.twig", "/home/wilder/PhpstormProjects/wild-series/templates/base.html.twig");
     }
