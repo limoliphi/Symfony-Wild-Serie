89f41657b6db3632d99f79c7abe06b7d750af10c
Lilian Maurin <lilianmaurin.wcs2019@gmail.com>
2019-11-22 10:58:38 +0100

    rectif program

--- src/Entity/Program.php
+++ src/Entity/Program.php
@@ -27,6 +27,21 @@ class Program
      */
     private $category;
 
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $summary;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $poster;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -52,6 +67,42 @@ class Program
     public function setCategory(?Category $category): self
     {
         $this->category = $category;
+
+        return $this;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(?string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getSummary(): ?string
+    {
+        return $this->summary;
+    }
+
+    public function setSummary(?string $summary): self
+    {
+        $this->summary = $summary;
+
+        return $this;
+    }
+
+    public function getPoster(): ?string
+    {
+        return $this->poster;
+    }
+
+    public function setPoster(string $poster): self
+    {
+        $this->poster = $poster;
 
         return $this;
     }
